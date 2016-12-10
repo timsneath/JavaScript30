e70b97ac8352f71ffd0f0bb356cfd2301aad1917
Tim Sneath <tim@sneath.org>
2016-12-09 20:01:27 -0800

    lesson 1 implementation

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,29 @@
+function windowLoaded () {
+  const keys = document.querySelectorAll('.key')
+  console.log(keys)
+  keys.forEach(key =>
+    key.addEventListener('transitionend', removePlayingTransition))
+
+  window.addEventListener('keydown', keyDown)
+}
+
+function keyDown (e) {
+  const keyPressed = e.keyCode
+  const audio = document.querySelector(`audio[data-key="${keyPressed}"]`)
+
+  if (audio) {
+    // start keycap transition
+    var keycap = document.querySelector(`.key[data-key="${keyPressed}"]`)
+    keycap.classList.add('playing')
+
+    audio.currentTime = 0
+    audio.play()
+  }
+}
+
+function removePlayingTransition (e) {
+  console.log(e.target)
+  e.target.classList.remove('playing')
+}
+
+window.addEventListener('load', windowLoaded)
